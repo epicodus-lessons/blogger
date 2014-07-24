7fb3de16cc652fecbc721c2818b20c721b3531a8
michael <student@epicodus.com>
2014-07-24 16:22:30 -0700

    add post resource

--- router.js
+++ router.js
@@ -1,5 +1,6 @@
 Blogger.Router.map(function() {
   this.resource('posts', {path: '/'});
+  this.resource('post', {path: ':post_id'});
   this.resource('recent-comments');
   this.resource('about');
   this.resource('contact', function() {
