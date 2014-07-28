e7f4c53bb1adf3826c7756cb71aee24d6d7b178d
michael <michaelrkn@gmail.com>
2014-07-28 11:54:10 -0700

    make templates a little prettier

--- controllers/about.js
+++ controllers/about.js
@@ -1,6 +1,9 @@
 Blogger.AboutController = Ember.Controller.extend({
   isAuthorShowing: false,
   actions: {
+    showRealName: function() {
+      alert("Dracula is fictional, but was inspired by the 15th-century Romanian general and Wallachian Prince Vlad III the Impaler.")
+    },
     showAuthor: function() {
       this.set('isAuthorShowing', true);
     },
